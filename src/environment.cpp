38ecc640ae4e172af9a3ef78545d38d36ed554f1
Markus Mayer <widemeadows@gmail.com>
2020-05-08 19:32:30 +0200

    Add doxy comments and update C++ code style

--- src/environment.cpp
+++ src/environment.cpp
@@ -1,25 +1,25 @@
-/* \author Aaron Brown */
-// Create simple 3d highway enviroment using PCL
-// for exploring self-driving car sensors
+/*
+ * @ingroup environment
+ * @brief Create simple 3d highway environment using PCL for exploring self-driving car sensors.
+ *
+ * \author Markus Mayer
+ * \author Aaron Brown
+ * */
 
 #include "sensors/lidar.h"
 #include "render/render.h"
 #include "processPointClouds.h"
-// using templates for processPointClouds so also include .cpp to help linker
-#include "processPointClouds.cpp"
+#include "processPointClouds.cpp"  // using templates for processPointClouds so also include .cpp to help linker
+
 
 std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr &viewer) {
 
-    Car egoCar(Vect3(0, 0, 0), Vect3(4, 2, 2), Color(0, 1, 0), "egoCar");
-    Car car1(Vect3(15, 0, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car1");
-    Car car2(Vect3(8, -4, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car2");
-    Car car3(Vect3(-12, 4, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car3");
+    Car egoCar{Vect3{0, 0, 0}, Vect3{4, 2, 2}, Color{0, 1, 0}, "egoCar"};
+    Car car1{Vect3{15, 0, 0}, Vect3{4, 2, 2}, Color{0, 0, 1}, "car1"};
+    Car car2{Vect3{8, -4, 0}, Vect3{4, 2, 2}, Color{0, 0, 1}, "car2"};
+    Car car3{Vect3{-12, 4, 0}, Vect3{4, 2, 2}, Color{0, 0, 1}, "car3"};
 
-    std::vector<Car> cars;
-    cars.push_back(egoCar);
-    cars.push_back(car1);
-    cars.push_back(car2);
-    cars.push_back(car3);
+    std::vector<Car> cars { egoCar, car1, car2, car3 };
 
     if (renderScene) {
         renderHighway(viewer);
@@ -53,33 +53,33 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer) {
 void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr &viewer) {
 
     viewer->setBackgroundColor(0, 0, 0);
-
-    // set camera position and angle
     viewer->initCameraParameters();
+
     // distance away in meters
-    int distance = 16;
+    const int distance = 16;
 
     switch (setAngle) {
-        case XY :
+        case XY:
             viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0);
             break;
-        case TopDown :
+        case TopDown:
             viewer->setCameraPosition(0, 0, distance, 1, 0, 1);
             break;
-        case Side :
+        case Side:
             viewer->setCameraPosition(0, -distance, 0, 0, 0, 1);
             break;
-        case FPS :
+        case FPS:
             viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
     }
 
-    if (setAngle != FPS)
+    if (setAngle != FPS) {
         viewer->addCoordinateSystem(1.0);
+    }
 }
 
 
 int main(int argc, char **argv) {
-    std::cout << "starting enviroment" << std::endl;
+    std::cout << "starting environment" << std::endl;
 
     pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
     CameraAngle setAngle = XY;
